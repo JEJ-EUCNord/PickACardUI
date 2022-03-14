c90f1a5159b059197168d2281c18f8d48234ccac
Jens Jensen <jej@eucnord.dk>
2022-03-14 12:20:14 +0100

    Add project files.

---
+++ PickACardUI/CardPicker.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PickACardUI
+{
+    public class CardPicker
+    {
+        static Random random = new Random();
+
+        public static string[] PickSomeCards(int numberOfCards)
+        {
+            String[] pickedCards = new String[numberOfCards];
+            for (int i = 0; i < numberOfCards; i++)
+            {
+                pickedCards[i] = RandomSuit() + " " + RandomValue();
+            }
+            return pickedCards;
+        }
+
+        private static string RandomSuit()
+        {
+            // Skaf et tilfældigt tal mellem 1 og 4
+            int value = random.Next(1, 5);
+            // Er tallet 1, så returner Spar
+            if (value == 1)
+            {
+                return "Spar";
+            }
+            // Er tallet 2, så returner Hjerter
+            if (value == 2)
+            {
+                return "Hjerter";
+            }
+            // Er tallet 3, så returner Klør
+            if (value == 1)
+            {
+                return "Klør";
+            }
+            // Har vi ikke allerede returneret en værdi, er der kun Ruder tilbage
+            return "Ruder";
+        }
+
+        private static string RandomValue()
+        {
+            int value = random.Next(1, 14);
+            if (value == 1) return "Es";
+            if (value == 11) return "Knægt";
+            if (value == 12) return "Dronning";
+            if (value == 13) return "Konge";
+            return value.ToString();
+        }
+    }
+}
---
+++ PickACardUI/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PickACardUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            // Læs positionen af skyderen numberOfCards og omsæt denne til et heltal
+            int antalKort = (int)numberOfCards.Value;
+
+            // Træk det ønskede antal kort, og gem dem i tabbellen mineKort
+            string[] mineKort = CardPicker.PickSomeCards(antalKort);
+            
+            // Listboxen i vores GUI skal renses for tidligere værdier
+            listOfCards.Items.Clear();  
+
+            // Fyld de trukne kort over i listboxen i vores GUI
+            foreach(string kort in mineKort)
+            {
+                listOfCards.Items.Add(kort);
+            }
+        }
+    }
+}
